f06faf161403277a8d2afc46d4c228223ee9aa90
fishinthesea <18779186074@163.com>
2017-11-27 23:57:25 +0800

    eslint 設置

---
+++ src/components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>{{msg}}</div>
+        <ul>
+            <li><img src="" alt=""></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'this is my demo'
+            }
+        }
+    }
+</script>
